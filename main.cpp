b2613db19f800274efcc9d928df542a318515577
Semih <120328104+soztyurk@users.noreply.github.com>
2026-02-27 17:13:46 +0300

    Remove tip comment for running code
    Removed a tip comment about running code.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 int main() {
     // Q1 ****************************
     string name;
